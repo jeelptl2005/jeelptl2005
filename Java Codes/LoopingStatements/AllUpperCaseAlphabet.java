038c3e04e33354f125577cc61af0154eb3bb0228
JEEL PATEL <jeelptl2005@gmail.com>
2025-08-08 15:54:29 +0530

    Delete Java Codes directory

--- Java Codes/LoopingStatements/AllUpperCaseAlphabet.java
+++
@@ -1,9 +0,0 @@
-package LoopingStatements;
-
-public class AllUpperCaseAlphabet {
-    public static void main(String[] args) {
-        for(char i='A';i<='Z';i++){
-            System.out.print(i+" ");
-        }
-    }
-}
